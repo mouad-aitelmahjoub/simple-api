34a5613b83d532af69f762521fec980032e9cbd2
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-11-03 10:54:00 +0100

    Db connection 👌

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express")
 const app = express()
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
+dotenv.config()
 app.use(express.json())
 
-app.listen(3000)
+//------ DB config ------
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB ✨🎉🎊"))
+  .catch((err) => console.log(err))
+
+app.listen("5000", () => {
+  console.log("Backend is Running 🟢🟢🟢")
+})
